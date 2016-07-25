06a54cf2a67c25628bdf2d1cab9321b774efdf16
Burii <buriihenry@gmail.com>
2016-07-25 00:07:46 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blurit Computers/Home</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+   
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="Placeorder.php">place order</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                 <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="Laptops.php">Laptops</a>
+                    </li>
+                    <li>
+                        <a href="Desktops.php">Desktops</a>
+                    </li>
+                    <li>
+                        <a href="MackBooks.php">Mack Books</a>
+                    </li>
+                    <li>
+                        <a href="Accessories.php">Accessories</a>
+                    </li>
+                    <li>
+                        <a href="Aboutus.php">About Us</a>
+                    </li>
+                       <li>
+                        <a href="Contacts.php">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/dk5.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>Blurit Co</h1>
+                        <hr class="small">
+                        <span class="subheading">Best Quality products at affordable prices</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="col-lg-12 col-lg-offset-0">
+                <div class="col-lg-12 col-lg-offset-0" style="background-image: url('img/blu2light.jpg')">
+                    <div id="carousel-example-generic" class="carousel slide" >
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden-xs">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <center>
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="img-responsive img-full" src="blurit1wide.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="blurit1wide.jpg" alt="">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive img-full" src="blurit1wide.jpg" alt="">
+                            </div>
+                        </div>
+                        </center>
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                            <span class="icon-prev"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                            <span class="icon-next"></span>
+                        </a>
+                    </div>
+                </div><br>
+         <div class="container">
+        <div class="row">
+                <p> <font color="red">Welcome to Blurit Computers </font> </p>
+
+                     <table class="text-center" width="100%"cellpadding="20">
+                         <tr>
+
+                          <img src="best.jpg"  width="200">
+                         <img src="great.jpg" width="200">
+                           <img src="mpesa.jpg" width="250">
+                         <img src="genuine.jpg" width="200">
+                            <img src="best.jpg"  width="200">
+                       
+                         
+                  </tr>
+                
+
+                  </table>
+
+                <table class="text-center" width="100%"cellpadding="20">
+                    <tr >
+                        <td><a href="Laptops.php"><img src="g50.jpg" width="100"></a></td>
+                        
+                        <td ><a href="Desktops.php"><img src="hpdk2.jpg" width="100"></a></td>
+
+                        <td><a href="Mackbooks.php"><img src="mac3.jpg" width="100"></a></td>
+                        <td><a href="Accessories.php"><img src="mouse2.jpg" width="100"></a></td>
+                    </tr>
+                    <tr>
+               
+                        <td><a href="Laptops.php"> Laptops</a></td>
+                        <td><a href="Desktops.php"> Desktops</a></td>
+                        <td><a href="MackBooks.php"> Mack Books</a></td>
+                        <td><a href="Accessories.php">  Accessories</a></td>
+                       
+                         
+                    </tr>
+
+                </table>
+                <table class="text-center" width="100%"cellpadding="20">
+                    <tr >
+                        <td><a href="Placeorder.php"><img src="3.jpg" width="100"></a></td>
+                        
+                        <td ><a href="Aboutus.php"><img src="2.jpg" width="100"></a></td>
+
+                        <td><a href="Contacts.php"><img src="1.jpg" width="100"></a></td>
+                        <td><a href="index.php"><img src="blurit1.jpg" width="100"></a></td>
+                    </tr>
+                    <tr>
+               
+                        <td><a href="Placeorder.php">Place Order</a></td>
+                        <td><a href="Aboutus.php">About Us</a></td>
+                        <td><a href="Contacts.php">Contact Us</a></td>
+                        <td><a href="index.php">Home</a></td>
+                       
+                         
+                    </tr>
+
+                </table> 
+                    <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1">
+                    <img src="air5.jpg"  width="200">
+                    <img src="conn.jpg" width="200">
+                    <img src="free.jpg" width="200">
+                    <img src="envys.png" width="100%">
+                   </div>
+
+                </div>
+
+                </div>
+                  
+               </div>
+               <ul class="pager">
+                    <li class="next">
+                    
+                           
+                        <a href="#">Back to top &rarr;</a>
+                    </li>
+                </ul>
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p  class="copyright text-muted"><font color="blue">Copyright &copy; Blurit Co 2016</font></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
